371ded327b5cc9cd66603b166dc140b4abae0ff6
chandrakiran-1 <gudlachandrakiran@gmail.com>
2025-12-05 10:10:46 +0530

    Added PrimeNumber code

--- MaxinArray.java
+++ MaxinArray.java
@@ -1,3 +1,27 @@
-void main() {
-  $END$
+import java.util.*;
+
+class MaxInArray {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.println("Enter Size of an Array:");
+        int n = x.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for(int i = 0; i < n; i++) {    // start from 0, go to n-1
+            arr[i] = x.nextInt();
+        }
+
+        int max = arr[0];               // assume first element is max
+
+        for(int i = 1; i < n; i++) {    // start from 1, go to n-1
+            if(arr[i] > max) {
+                max = arr[i];           // update max if bigger
+            }
+        }
+
+        System.out.println("Max Number: " + max);
+    }
 }
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,3 +1,25 @@
-void main() {
-  $END$
+import java.util.*;
+
+class PrimeNumber {
+    public static void main(String[] args) {
+
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = a.nextInt();
+
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }                                               // count is used to define the divisor.
+
+        if (count == 2) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+    }
+
 }
