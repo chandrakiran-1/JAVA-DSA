7c52c20e7064a09d0725ef11ebbcadfe7e3c427f
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-01-24 05:45:10 +0530

    Updated ALLARRAYLISTMETHODS with full ArrayList code

--- java/src/ARRAYS/ALLARRAYLISTMETHODS.java
+++ java/src/ARRAYS/ALLARRAYLISTMETHODS.java
@@ -1,4 +1,24 @@
 package ARRAYS;
 
-public class ALLARRAYMETHODS {
+import java.util.ArrayList;
+
+public class ALLARRAYLISTMETHODS {
+    public static void main(String[]args)
+    {
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list.get(2));
+        list.set(2,99);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.remove(1));
+        System.out.println(list.contains(99));
+        list.clear();
+        System.out.println(list);
+
+
+    }
 }
