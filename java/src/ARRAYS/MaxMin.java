ee799d2040df23b55b51a51bb34de7b35e44a7d6
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-02-06 13:22:49 +0530

    restarted

--- java/src/ARRAYS/MaxMin.java
+++ java/src/ARRAYS/MaxMin.java
@@ -1,4 +1,31 @@
 package ARRAYS;
+import java.util.*;
 
 public class MaxMin {
+    public static void main(String[]args)
+    {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter the Array Size :  ");
+        int size=x.nextInt();
+        int [] arr= new int [size];
+        System.out.println("Enter the Array Elements : ");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=x.nextInt();
+        }
+        int max=arr[0];
+        int min=arr[0];
+        for( int i=0;i<arr.length;i++)
+        {
+            if(arr[i]> max)
+            {
+                max=arr[i];
+            } else if (arr[i]< min) {
+                min=arr[i];
+
+            }
+        }
+        System.out.println("Max Element : "+ max);
+System.out.println("Min Element : " + min);
+}
 }
