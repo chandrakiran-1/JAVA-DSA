3cf3447d255255004f1515322afd8480e8ad77fd
chandrakiran-1 <gudlachandrakiran@gmail.com>
2026-02-09 21:14:20 +0530

    Reversed

--- java/src/ARRAYS/MinMax.java
+++ java/src/ARRAYS/MinMax.java
@@ -5,32 +5,34 @@ public class MinMax {
     public static void main(String[]args)
     {
         Scanner x = new Scanner(System.in);
-        System.out.print("enter a Array Size : ");
-        int size= x.nextInt();
+        System.out.println("Enter the size of the Array : ");
+        int size = x.nextInt();
         System.out.println("Enter the Array Elements : ");
         int []arr;
         int max;
-        int  min;
+        int min;
         arr= new int[size];
         for(int i=0;i<arr.length;i++)
         {
             arr[i]=x.nextInt();
         }
-        max=arr[0];
+        max= arr[0];
         min=arr[0];
         for(int i=1;i<arr.length;i++)
         {
             if(arr[i] > max)
             {
                 max=arr[i];
+
             }
-            else if (arr[i]<min)
+            else if( arr[i] < min)
             {
-                min= arr[i];
+                min=arr[i];
+
             }
         }
-        System.out.println("Max : "  + max);
-        System.out.println("Min : "   + min);
+        System.out.println("Max : " + max);
+        System.out.println("Min : " + min);
 
 
     }
---
+++ java/src/ARRAYS/Reverse.java
@@ -0,0 +1,36 @@
+package ARRAYS;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[]args)
+    {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter Array Size : ");
+        int size= x.nextInt();
+        int []arr;
+        arr= new int [size];
+        System.out.println("Enter the Array Elements : ");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=x.nextInt();
+        }
+        int start=0;
+        int end= arr.length-1;
+        while(start < end)
+        {
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+
+        }
+        System.out.println("Reversed Array : ");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
